799aa94fa9c0b06dfa4e4a462846787d612dc5cf
renancrociari <renanone@gmail.com>
2026-02-14 22:21:03 +0000

    Update case url

--- src/scripts/password-config.js
+++ src/scripts/password-config.js
@@ -19,13 +19,9 @@
 // Password hashes (SHA-256)
 // Default password for demo: "demo123"
 export const PASSWORD_CONFIG = {
-    'case-study-1': {
-        hash: 'd3ad9315b7be5dd53b31a273b3b3aba5defe700808305aa16a3062b76658a791',
-        redirectUrl: '/improving-the-performance-of-farfetchs-top-fashion-ecommerce-brands.html'
-    },
     'case-journal-finder': {
         hash: 'e4e2ff1a6fc2a6ce836b66c424aa2bdd4803d5b92d9750b0db4df4b1d1a8a597',
-        redirectUrl: '/connecting-researchers-with-the-perfect-home-for-scientific-discoveries.html'
+        redirectUrl: '/connecting-every-discovery-with-a-worthy-home'
     }
 };
 
